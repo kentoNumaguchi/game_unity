eb410ff1bc6bb70690eb6f0bd9439aa29d94ca6c
沼口 健人 <k.numaguchi1031@gmail.com>
2012-12-16 13:52:10 +0900

    コントローラー作成

---
+++ Assets/contlloer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class contlloer : MonoBehaviour {
+	private int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
